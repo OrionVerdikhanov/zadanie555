65130e9658a4268192683fe9bb13d625671c2892
Feitulla <113803928+OrionVerdikhanov@users.noreply.github.com>
2024-12-29 13:49:03 +0300

    Create task4_options_flags.py

---
+++ task4_options_flags.py
@@ -0,0 +1,31 @@
+import argparse
+
+def main():
+    # Создание парсера аргументов
+    parser = argparse.ArgumentParser(
+        description='Процессинг числа и строки с дополнительными опциями.'
+    )
+
+    # Добавление обязательных аргументов
+    parser.add_argument('number', type=int, help='Число для вывода')
+    parser.add_argument('text', type=str, help='Строка для вывода')
+
+    # Добавление опций (флагов)
+    parser.add_argument('--verbose', action='store_true',
+                        help='Вывод дополнительной информации')
+    parser.add_argument('--repeat', type=int, default=1,
+                        help='Количество повторений строки')
+
+    # Парсинг аргументов
+    args = parser.parse_args()
+
+    # Вывод дополнительной информации, если опция verbose установлена
+    if args.verbose:
+        print(f'Полученные аргументы: number={args.number}, '
+              f'text="{args.text}", repeat={args.repeat}')
+
+    # Вывод строки, повторенной указанное количество раз
+    print(f'Число: {args.number}, Строка: {args.text * args.repeat}')
+
+if __name__ == '__main__':
+    main()
